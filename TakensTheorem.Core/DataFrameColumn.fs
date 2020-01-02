0baf2a448118e9e78aaf8c68cf30fe2258e3e570
Ares Lazarus <cernunos_kav@msn.com>
2020-01-03 00:29:18 +0200

    implemented mutual information function

---
+++ TakensTheorem.Core/DataFrameColumn.fs
@@ -0,0 +1,162 @@
+namespace TakensTheorem.Core
+
+open System
+open XPlot.Plotly
+open Microsoft.Data.Analysis
+open System.Collections.Generic
+open System.IO
+open DataFrameColumnOperators
+
+type DataFrameColumn<'T when 'T :> ValueType and 'T: struct and 'T: (new: unit -> 'T)> private () =
+
+    static member Values(source: PrimitiveDataFrameColumn<'T>) =
+        source
+        |> Seq.cast<Nullable<'T>>
+        |> Array.ofSeq
+
+    static member Slice(start: int64, stop: int64) =
+        fun (source: PrimitiveDataFrameColumn<'T>) ->
+            let data = Array.init (int (stop - start) + 1) (fun i -> source.[start + int64 i])
+            PrimitiveDataFrameColumn<'T>(source.Name, data)
+
+    static member Trim(trimValue: 'T) =
+        fun (source: PrimitiveDataFrameColumn<'T>) ->
+            let trimValue = Nullable trimValue
+            let mutable start = 0L
+            let mutable stop = source.Length - 1L
+
+            while source.[start] = trimValue do
+                start <- start + 1L
+            while source.[stop] = trimValue do
+                stop <- stop - 1L
+
+            let trimmed = (source |> DataFrameColumn.Values).[int start..int stop]
+
+            PrimitiveDataFrameColumn<'T>(source.Name, trimmed)
+
+    static member FromValues (name: string) (values: 'T seq) = PrimitiveDataFrameColumn<'T>(name, values)
+
+    static member CreateFilter (predicate: Nullable<'T> -> bool) (source: PrimitiveDataFrameColumn<'T>) =
+        source
+        |> Seq.cast<Nullable<'T>>
+        |> Seq.map predicate
+        |> DataFrameColumn.FromValues(sprintf "%s filter" source.Name)
+
+    static member Plot(source: PrimitiveDataFrameColumn<'T>) = Scatter(y = DataFrameColumn.Values source) |> Chart.Plot
+
+[<RequireQualifiedAccess>]
+module StringDataFrameColumn =
+    let Values(source: StringDataFrameColumn) =
+        source
+        |> Seq.cast<string>
+        |> Array.ofSeq
+
+    let FromValues (name: string) (values: string seq) = StringDataFrameColumn(name, values)
+
+    let Slice (start: int64, stop: int64) (source: StringDataFrameColumn) =
+        let data = Array.init (int (stop - start) + 1) (fun i -> source.[start + int64 i])
+        StringDataFrameColumn(source.Name, data)
+
+    let Trim (trimValue: string) (source: StringDataFrameColumn) =
+        let mutable start = 0L
+        let mutable stop = source.Length - 1L
+
+        while source.[start] = trimValue do
+            start <- start + 1L
+        while source.[stop] = trimValue do
+            stop <- stop - 1L
+
+        //let trimmed = (source |> AsArrayOfString).[int start..int stop]
+        let trimmed = Array.init (int (stop - start) + 1) (fun n -> source.[start + int64 n])
+
+        StringDataFrameColumn(source.Name, trimmed)
+
+    let CreateFilter (predicate: string -> bool) (source: StringDataFrameColumn) =
+        source
+        |> Seq.cast<string>
+        |> Seq.map predicate
+        |> DataFrameColumn.FromValues(sprintf "%s filter" source.Name)
+
+
+[<RequireQualifiedAccess>]
+module DataFrameColumn =
+    open DataFrameColumnOperators
+
+    let Rolling<'T when 'T :> ValueType> windowSize (groupFunction: 'T [] -> 'T) (source: DataFrameColumn) =
+        source
+        |> Seq.cast<'T>
+        |> Seq.windowed windowSize
+        |> Seq.map groupFunction
+
+    let TakensEmbedding<'T when 'T: equality and 'T :> ValueType and 'T: (new: unit -> 'T) and 'T: struct> (delay: int)
+        (dimension: int) (data: PrimitiveDataFrameColumn<'T>) =
+        (* This function returns the Takens embedding of data with delay into dimension,
+         * delay*dimension must be < len(data)
+         *)
+        data
+        |> DataFrameColumn.Values
+        |> Takens.Embedding delay dimension
+
+    let Filter (filter: PrimitiveDataFrameColumn<bool>) (source: DataFrameColumn) = source.Clone(filter)
+
+    let Length(source: DataFrameColumn) = source.Length
+
+    let MutualInformation delay (nBins: int) (data: PrimitiveDataFrameColumn<float>) =
+        let mutable I = 0.0
+
+        let xmax = data.Max() :?> float
+        let xmin = data.Min() :?> float
+
+        let length = data.Length
+        let delayData = data |> DataFrameColumn.Slice(delay, length - 1L)
+        let shortData = data |> DataFrameColumn.Slice(0L, length - delay - 1L)
+        let sizeBin = Math.Abs(xmax - xmin) / float nBins
+        let probInBin = Dictionary<float, float>()
+        let conditionBin = Dictionary<float, PrimitiveDataFrameColumn<bool>>()
+        let conditionDelayBin = Dictionary<float, PrimitiveDataFrameColumn<bool>>()
+
+        let range = [ 0 .. nBins - 1 ] |> List.map float
+        for h in range do
+            if (probInBin.ContainsKey >> not) h then
+                //let sub1 = shortData.ElementwiseGreaterThanOrEqual(xmin + h * sizeBin)
+                //let sub2 = shortData.ElementwiseLessThan(xmin + (h + 1.) * sizeBin)
+                //conditionBin.Add(h, sub1.And(sub2) :?> PrimitiveDataFrameColumn<bool>)
+
+                let filter = (shortData />= (xmin + h * sizeBin)) /& (shortData /< (xmin + (h + 1.) * sizeBin))
+                conditionBin.Add(h, filter)
+
+                //let pib = float ((!>! shortData).Clone(filter).Length) / float shortData.Length
+                probInBin.Add
+                    (h,
+                     (shortData
+                      |> Filter filter
+                      |> Length
+                      |> float)
+                     / float shortData.Length)
+
+            for k in range do
+                if (probInBin.ContainsKey >> not) k then
+                    let filter = (shortData />= (xmin + k * sizeBin)) /& (shortData /< (xmin + (k + 1.) * sizeBin))
+                    conditionBin.Add(k, filter)
+                    probInBin.Add
+                        (k,
+                         (shortData
+                          |> Filter filter
+                          |> Length
+                          |> float)
+                         / float shortData.Length)
+
+                if (conditionDelayBin.ContainsKey >> not) k then
+                    conditionDelayBin.Add
+                        (k, (delayData />= (xmin + k * sizeBin)) /& (delayData /< (xmin + (k + 1.) * sizeBin)))
+                let Phk =
+                    float shortData.Length
+                    |> (/)
+                    <| shortData
+                    |> Filter(conditionBin.[h] /& conditionDelayBin.[k])
+                    |> Length
+                    |> float
+
+                if Phk <> 0. && probInBin.[h] <> 0. && probInBin.[k] <> 0. then
+                    I <- I - (Phk * Math.Log(Phk / (probInBin.[h] * probInBin.[k])))
+        I
